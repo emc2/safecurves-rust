611bc343bc8792cf3dc5d01592fcc99f27da51ce
Eric McCorkle <eric@metricspace.net>
2017-04-14 11:56:00 -0400

    Add branchless Montgomery ladder

--- src/curve/edwards.rs
+++ src/curve/edwards.rs
@@ -70,11 +70,82 @@ impl<C : EdwardsCurve> Add<EdwardsExtended<C>> for EdwardsExtended<C> {
 impl<C: EdwardsCurve> Point for EdwardsExtended<C> {
     type Scalar = C::Scalar;
 
-    fn init(&mut self, x: Self::Scalar, y: Self::Scalar) {
+    fn zero() -> Self {
+        EdwardsExtended {
+            x: C::Scalar::zero(),
+            y: C::Scalar::one(),
+            z: C::Scalar::one(),
+            t: C::Scalar::one()
+        }
+    }
+
+    fn init(&mut self, x: C::Scalar, y: C::Scalar) {
         self.x = x;
         self.y = y;
         self.z = C::Scalar::one();
         self.t = C::Scalar::one();
+    }
+
+    fn scalar_mult(&mut self, rhs: &mut Self::Scalar) {
+        rhs.normalize();
+        self.scalar_mult_normalized(rhs);
+    }
+
+    fn scalar_mult_normalized(&mut self, rhs: &Self::Scalar) {
+        let mut r0 = Self::zero();
+        let mut r1 = self.clone();
+        let nbits = C::Scalar::nbits();
+
+        for i in 0..nbits {
+            let idx = nbits - 1 - i;
+            let bit = rhs.bit_normalized(idx);
+            let tmask = C::Scalar::filled(bit);
+            let fmask = C::Scalar::filled(!bit);
+            let mut r0d = r0.doubled();
+            let mut r1d = r1.doubled();
+
+            r0 += r1;
+            r0.x.normalize();
+            r0.y.normalize();
+            r0.z.normalize();
+            r0.t.normalize();
+            r1 = r0;
+
+            r0.x.normalized_bitand(&fmask);
+            r0.y.normalized_bitand(&fmask);
+            r0.z.normalized_bitand(&fmask);
+            r0.t.normalized_bitand(&fmask);
+
+            r1.x.normalized_bitand(&tmask);
+            r1.y.normalized_bitand(&tmask);
+            r1.z.normalized_bitand(&tmask);
+            r1.t.normalized_bitand(&tmask);
+
+            r0d.x.normalize_self_bitand(&tmask);
+            r0d.y.normalize_self_bitand(&tmask);
+            r0d.z.normalize_self_bitand(&tmask);
+            r0d.t.normalize_self_bitand(&tmask);
+
+            r1d.x.normalize_self_bitand(&fmask);
+            r1d.y.normalize_self_bitand(&fmask);
+            r1d.z.normalize_self_bitand(&fmask);
+            r1d.t.normalize_self_bitand(&fmask);
+
+            r0.x.normalized_bitor(&r0d.x);
+            r0.y.normalized_bitor(&r0d.y);
+            r0.z.normalized_bitor(&r0d.z);
+            r0.t.normalized_bitor(&r0d.t);
+
+            r1.x.normalized_bitor(&r1d.x);
+            r1.y.normalized_bitor(&r1d.y);
+            r1.z.normalized_bitor(&r1d.z);
+            r1.t.normalized_bitor(&r1d.t);
+        }
+
+        self.x = r0.x;
+        self.y = r0.y;
+        self.z = r0.z;
+        self.t = r0.t;
     }
 
     fn double(&mut self) {
--- src/curve/point.rs
+++ src/curve/point.rs
@@ -6,8 +6,16 @@ use std::ops::AddAssign;
 pub trait Point : Add<Self, Output = Self> + AddAssign<Self> + Copy {
     type Scalar : PrimeField;
 
+    fn zero() -> Self;
+
     /// In-place constructor for points from x-y coordinates.
     fn init(&mut self, x: Self::Scalar, y: Self::Scalar);
+
+    /// Scalar multiplication.
+    fn scalar_mult(&mut self, rhs: &mut Self::Scalar);
+
+    /// Scalar multiplication by a normalized scalar.
+    fn scalar_mult_normalized(&mut self, rhs: &Self::Scalar);
 
     /// Double a point in place.
     fn double(&mut self);
